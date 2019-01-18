14e8feaf9fe17d0dca9e697c6e35437d7f30104c
Andrew-Sher <44896044+Andrew-Sher@users.noreply.github.com>
2019-01-18 17:22:41 +0300

    H/w JS#1

--- js/Example5.js
+++ js/Example5.js
@@ -1,15 +1,18 @@
-function isEven (n)  {
-    let a = n;
-    if (n % 2 === 0) {
-        for (let i = 0;  i < n; i++) {
-         a -= 1;
-        }
-        return a ;
-    } else {
-        for (let i=0; i < n; i++) {
-            a -= 1;
-        }
-        return a + 1 ;
-    } 
+// function isEven (n)  {
+//     let a = n;
+//     if (n % 2 === 0) {
+//         return n - isEven( n-1) ;
+//     } else {
+//         for (let i=0; i < n; i++) {
+//             a -= 1;
+//         }
+//         return a + 1 ;
+//     } 
+// }
+function isEven(n) {
+	if (n == 0) return 0;
+    else if (n == 1) return 1;
+    else if ( n < 0 ) return isEven(-n);
+    else return isEven(n - 2);
 }
-console.log(isEven(-1));
+console.log(isEven(-5));
--- js/Example7.js
+++ js/Example7.js
@@ -1,13 +1,8 @@
-function sumArr (start, finish, step = star < end ? 1 : -1 ) {
+function sumArr (start, finish, step) {
     let sum = 0;
     let arr = [];
     if (step === undefined) {
         step = 1 ;
-    }
-    else if ( step < 0) {
-        for (let i = finish; i >= start; i+= step){
-        arr.push(i);
-    }
     }
     for (let i = start; i <= finish; i += step){
         arr.push(i);
@@ -17,4 +12,4 @@ function sumArr (start, finish, step = star < end ? 1 : -1 ) {
     }
     return sum;
 }
-console.log(sumArr(1,10, -1));
+console.log(sumArr(1,10, 2));
